00a5f2ed623ad4f5c998bcc87d857fde7a1b8975
lacoleman0101@gmail.com <locomama@DESKTOP-F8BQ2UB.localdomain>
2022-07-24 17:24:14 -0500

    commit tree e9aafcb590807a672658832641add0a5606bbd3e

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
-const name = "Joe";
+const name = "Susan";
 const height = 74;
 const message = `${name} is ${height} inches tall`;
 
 module.exports = {name, height, message};
+console.log(message);
+
+
+
+
+
